e57a755925f4dba7e1ce98844b77688bcef84489
appinventiv <appinventiv@555-mini.local>
2017-09-20 15:19:20 +0530

    updated

--- SideMenuBar/SidemenuVc.swift
+++ SideMenuBar/SidemenuVc.swift
@@ -9,31 +9,33 @@
 import UIKit
 
 class SidemenuVc: UIViewController {
+    //-----------variable declartion--------//
     var menuShowing = false
-    
+    //-----------Outlets--------------//
     @IBOutlet var menuView: UIView!
     @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
-    
+    //-------------Button Actions----------//
     @IBAction func viewBtn(_ sender: UIButton) {
+        menuShowing = !menuShowing
+        leadingConstraint.constant = -150
         let obj = self.storyboard?.instantiateViewController(withIdentifier: "View2Id") as? View2Vc
         self.navigationController?.pushViewController(obj!, animated: true)
-//        self.navigationController?.popViewController(animated: true)
     }
     @IBAction func openmenuBtn(_ sender: Any) {
         if (menuShowing) {
             leadingConstraint.constant = -150
         }else{
-           leadingConstraint.constant = 0
+            leadingConstraint.constant = 0
             UIView.animate(withDuration: 0.5, animations: {
                 self.view.layoutIfNeeded()
-            })
-        }
-    menuShowing = !menuShowing
+})
+}
+        menuShowing = !menuShowing
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-menuView.layer.shadowOpacity = 1
+        menuView.layer.shadowOpacity = 1
         menuView.layer.shadowRadius = 6
         
     }
